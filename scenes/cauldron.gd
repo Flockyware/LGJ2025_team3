800a3c8a60605eb0dffbbba3623910df1f5425fc
Christopher Hayling <christopherhayling9@gmail.com>
2025-01-25 13:41:58 -0500

    animacion2

--- scenes/cauldron.gd
+++ scenes/cauldron.gd
@@ -12,6 +12,8 @@ var ingridients = {
 var pocimaA=$CanvasLayer/Control/VBoxContainer/buttons_container_up/Button_A
 @onready
 var animacionButtonA=$CanvasLayer/Control/VBoxContainer/buttons_container_up/Button_A/AnimationPlayer
+@onready
+var animacionButtonB=$CanvasLayer/Control/VBoxContainer/buttons_container_up/Button_B/AnimationPlayer
 
 func recipe_print():
 	print(recipe)
@@ -24,6 +26,7 @@ func _on_button_a_pressed() -> void:
 	
 
 func _on_button_b_pressed() -> void:
+	animacionButtonB.play("verterPocima2")
 	recipe.append(ingridients.b)
 	recipe_print()
 

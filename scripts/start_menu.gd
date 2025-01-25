ec448e51f1d100f33bbf3663db1629e0b1aee7ea
Moizez102 <moizez102@gmail.com>
2025-01-24 21:55:34 -0400

    Cambios menores al menu de inicio y comentarios en script

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
+#---------Esta funcion esta conectada a la señal "pressed" del boton start, te mueve a la escena "game"
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
+#---------Esta funcion esta conectada a la señal "pressed" del boton exit, cierra directamente el juego
 func _on_exit_pressed() -> void:
 	get_tree().quit()

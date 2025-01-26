570c9a532f9241da21057110cdc114a3551cc4eb
Christopher Hayling <christopherhayling9@gmail.com>
2025-01-26 17:58:35 -0500

    primera escena cliente

--- scripts/main_script.gd
+++ scripts/main_script.gd
@@ -3,7 +3,7 @@ extends Node
 @onready var transiciones:AnimationPlayer = $Transitions
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	transiciones.play("to_client")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
